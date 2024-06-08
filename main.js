058d9180ea9dcd9e6b1fa6d0522b926e35f2f4fa
eduardo diaz <trashpastard@gmail.com>
2024-06-08 13:47:18 -0600

    alerta para texto vacio

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let create = document.getElementById("create");
 let clear = document.getElementById("clear");
 let TaskList = document.getElementById("TaskList");
+let navUp = document.getElementById("navUp");
 
-
+let vacio = true;
 
 let textarea = document.querySelector("textarea");
 textarea.addEventListener("keyup", e=>{
@@ -20,18 +21,39 @@ clear.addEventListener("click", function (event) {
 
 create.addEventListener("click", function (event){
     event.preventDefault();
-    inValue = textInput.value;
-    
+    inValue = textInput.value;  
 
-let cardTask = document.createElement('div');
-cardTask.classList.add('TaskList');
-cardTask.innerHTML = `
-<div class="card text-bg-info mb-3 $cyan-100" style="max-width: 20rem;">
-<div class="card-body">
-  <p class="card-text">${inValue}</p>
-</div>
-`;
-    TaskList.appendChild(cardTask);
+   
+
+
+    if (textInput.value == "") {
+        let alerta = document.createElement('div');
+        alerta.classList.add('alert');
+        alerta.innerHTML=`
+        <div class="alert alert-danger" role="alert">
+         No has ingresado una tarea
+        </div>
+        `
+        navUp.appendChild(alerta);
+        //desaparecer alerta a los 5 segundos 
+        setTimeout(() => {
+            alerta.remove();
+        }, 1500);
+    } else {
+        let cardTask = document.createElement('div');
+        cardTask.classList.add('TaskList');
+        cardTask.innerHTML = `
+        <div class="card text-bg-info mb-3 $cyan-100" style="max-width: 20rem;">
+        <div class="card-body">
+            <p class="card-text">${inValue}</p>
+        </div>
+        `;
+        TaskList.appendChild(cardTask);
+        textInput.value = "";  
+        
+    }
+
+
 });
    
 
